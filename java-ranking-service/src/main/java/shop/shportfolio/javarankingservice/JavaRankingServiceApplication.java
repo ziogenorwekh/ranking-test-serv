06630afd58baa677cb30ca51cc074a89fcb88827
LSEK <ziogenorwekh@gmail.com>
2025-11-30 23:00:48 +0900

    initialize py+java project

---
+++ java-ranking-service/src/main/java/shop/shportfolio/javarankingservice/JavaRankingServiceApplication.java
@@ -0,0 +1,13 @@
+package shop.shportfolio.javarankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRankingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRankingServiceApplication.class, args);
+    }
+
+}
---
+++ java-ranking-service/src/test/java/shop/shportfolio/javarankingservice/JavaRankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.shportfolio.javarankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRankingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
